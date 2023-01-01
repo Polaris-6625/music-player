80e3238033dca53b467e62da5796fff3f8b451ed
liuyuyang <2281487673@qq.com>
2023-01-01 17:19:01 +0800

    n

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,7 @@
       :autoplay="data.autoplay"
       :loop="true"
       type="progress"
+      preload="true"
       @forward="forward"
       @fastBack="fastBack"
       @play="changeStatus"
@@ -111,7 +112,7 @@
     param.append("val_flag",val_flag);
     //调用方法
     proxy.$http
-      .post("http://127.0.0.1:8083/getPaging",param)
+      .post("http://polaris.lyyfsq.club:8083/getPaging",param)
       .then(function(res:any):void {
         console.log(res.data);
         menuList.value = res.data;
@@ -127,7 +128,7 @@
     param.append("val_flag","1");
     //调用方法
     proxy.$http
-      .post("http://127.0.0.1:8083/getPaging",param)
+      .post("http://polaris.lyyfsq.club:8083/getPaging",param)
       .then(function(res:any):void {
         console.log(res.data);
         menuList.value = res.data;
@@ -143,7 +144,7 @@
     data.autoplay = false;
     console.log(n);
     urlSend.value = "";
-    urlSend.value = urlSend.value + "http://127.0.0.1:8083/music?musicName=" + n;
+    urlSend.value = urlSend.value + "http://polaris.lyyfsq.club:8083/music?musicName=" + n;
     console.log(urlSend.value);
     // (audioRef.value as any).load();
     // (audioRef.value as any).play();
